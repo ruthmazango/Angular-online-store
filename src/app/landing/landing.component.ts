ba2aae5ccd2e45962b9d2365c733cef18b884776
ruthmazango <ruthmazango@gmail.com>
2022-05-22 18:47:14 +0200

    done

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css'],
+  selector: 'ngbd-carousel-basic', 
+  templateUrl: './carousel-basic.html'
+})
+
+export class LandingComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
